dc7c0b9ed3abe498b715161daae34312f84afa57
Yufan341955 <861487325@qq.com>
2016-11-04 19:48:42 +0800

    添加Log工具类并实现点击登录注册按钮页面跳转到相应页面

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -2,14 +2,40 @@ package cn.ucai.superwechat.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
 
+    Button mbtnLogin,mbtnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        initView();
+        setListener();
+    }
+
+    private void setListener() {
+        mbtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MFGT.gotoLogin(GuideActivity.this);
+            }
+        });
+        mbtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MFGT.gotoRegister(GuideActivity.this);
+            }
+        });
+    }
+
+    private void initView() {
+       mbtnLogin= (Button) findViewById(R.id.btn_Login);
+       mbtnRegister= (Button) findViewById(R.id.btn_Register);
     }
 }
