2b822b116cfd5ae61bcd8c9c44cd2075243d97ab
Yufan341955 <861487325@qq.com>
2016-11-08 09:07:03 +0800

    联系人列表隐藏原生titlebar

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ContactListFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ContactListFragment.java
@@ -65,7 +65,7 @@ public class ContactListFragment extends EaseContactListFragment {
         applicationItem = (ContactItemView) headerView.findViewById(R.id.application_item);
         applicationItem.setOnClickListener(clickListener);
         headerView.findViewById(R.id.group_item).setOnClickListener(clickListener);
-       headerView.findViewById(R.id.chat_room_item).setOnClickListener(clickListener);
+       //headerView.findViewById(R.id.chat_room_item).setOnClickListener(clickListener);
         headerView.findViewById(R.id.robot_item).setOnClickListener(clickListener);
         listView.addHeaderView(headerView);
         //add loading view
@@ -108,6 +108,7 @@ public class ContactListFragment extends EaseContactListFragment {
             }
         });
         //设置联系人数据
+        titleBar.setVisibility(View.GONE);
         Map<String, EaseUser> m = SuperWeChatHelper.getInstance().getContactList();
         if (m instanceof Hashtable<?, ?>) {
             m = (Map<String, EaseUser>) ((Hashtable<String, EaseUser>)m).clone();
@@ -185,10 +186,10 @@ public class ContactListFragment extends EaseContactListFragment {
                 // 进入群聊列表页面
                 startActivity(new Intent(getActivity(), GroupsActivity.class));
                 break;
-            case R.id.chat_room_item:
-                //进入聊天室列表页面
-                startActivity(new Intent(getActivity(), PublicChatRoomsActivity.class));
-                break;
+//            case R.id.chat_room_item:
+//                //进入聊天室列表页面
+//                startActivity(new Intent(getActivity(), PublicChatRoomsActivity.class));
+//                break;
             case R.id.robot_item:
                 //进入Robot列表页面
                 startActivity(new Intent(getActivity(), RobotsActivity.class));
